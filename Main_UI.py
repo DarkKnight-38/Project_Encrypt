6edcf3274b885ba9b57782e7fddf79c5b0ddb791
DarkKnight38 <raphaeladrian2909@gmail.com>
2026-08-31 22:10:52 +0530

    Re organised 	renamed:    Data/ActionHist_Stack/Action_Hist.py -> Functions/Action_Hist.py 	renamed:    Data/UserData/User_Auth.py -> Functions/User_Auth.py 	modified:   Main_UI.py

--- Main_UI.py
+++ Main_UI.py
@@ -5,13 +5,13 @@ from InquirerPy import inquirer
 from rich import print
 
 # Local Application Imports
-import Data.ActionHist_Stack.Action_Hist as Action_Hist
+import Functions.Action_Hist as Action_Hist
 import Functions.Encryptions as Encryptions
 import Functions.Decryption as Decryption
 import Functions.Graph as Graph
 import Functions.PW_Generator as PW_Generator
 import Functions.PW_Strength as PW_Strength
-import Data.UserData.User_Auth as User_Auth
+import Functions.User_Auth as User_Auth
 
 
 def main_menu():
